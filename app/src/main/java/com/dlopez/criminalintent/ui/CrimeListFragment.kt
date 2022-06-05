590f3835691ea549c3ba4759309d899f3d38b277
daen_ <daen.lopez14>
2022-06-05 12:59:22 -0600

    cleaning code

--- app/src/main/java/com/dlopez/criminalintent/ui/CrimeListFragment.kt
+++ app/src/main/java/com/dlopez/criminalintent/ui/CrimeListFragment.kt
@@ -33,7 +33,6 @@ class CrimeListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
         _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
 
         binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
@@ -50,28 +49,14 @@ class CrimeListFragment : Fragment() {
         viewLifecycleOwner.lifecycleScope.launch {
             //repeatOnLifecycle is itself a suspending function
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
-                //val crimes = crimeListViewModel.loadCrimes()
                 crimeListViewModel.crimes.collect {
                     updateUI(it)
                 }
                 //updateUI(crimes)
             }
         }
-        /*lifecycleScope.launch {
-
-        }*/
-
+        //lifecycleScope.launch {
         //lifecycle.currentState
-        //val crimes = crimeListViewModel.crimes
-        //updateUI(crimes)
-        /*crimeListViewModel.crimeListLiveData.observe(
-            viewLifecycleOwner,
-            Observer { crimes ->
-                crimes?.let {
-                    Log.i(TAG, "Got crimes ${crimes.size}")
-                    updateUI(crimes)
-                }
-            })*/
     }
 
     private fun updateUI(crimes: List<Crime>) {
@@ -90,57 +75,3 @@ class CrimeListFragment : Fragment() {
         }
     }
 }
-
-
-//RecyclerView expects an itemView to be wrapped in an instance of ViewHolder
-//A ViewHolder stores a reference to an item's view
-/*private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view),
-    View.OnClickListener {
-
-    private lateinit var crime: Crime
-    private val titleTextView = itemView.findViewById(R.id.crime_title) as TextView
-    private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
-    private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
-
-    init {
-        itemView.setOnClickListener(this)
-    }
-
-    override fun onClick(p0: View?) {
-        Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT).show()
-    }
-
-    fun bind(crime: Crime) {
-        this.crime = crime
-        titleTextView.text = this.crime.title
-        dateTextView.text =
-            SimpleDateFormat("EEEE, MMM dd, yyyy.", Locale.US).format(this.crime.date)
-        //dateTextView.text = this.crime.date.toString()
-        solvedImageView.visibility = if (crime.isSolved) {
-            View.VISIBLE
-        } else {
-            View.GONE
-        }
-    }
-
-}
-
-//A recyclerView creates ViewHolders, which brings their itemViews along**
-//A recyclerView asks the adapter to create a new ViewHolder
-//who or what creates crimeHolder instances? can I assume the view hierarchy passed in constructor to contain the child widget I expect?
-private inner class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
-        val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
-        return CrimeHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
-        val crime = crimes[position]
-        holder.bind(crime)
-    }
-
-    override fun getItemCount(): Int {
-        return crimes.size
-    }
-}*/
